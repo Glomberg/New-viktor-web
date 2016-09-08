72fde96f303e46ca977b86dafed67edd7d0ea8da
Glomberg <Bazz@bk.ru>
2016-09-08 16:31:21 +0300

    ewr

---
+++ www-custom/js/script.js
@@ -0,0 +1,29 @@
+﻿(function($){
+	
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(300).fadeOut('slow');
+	});
+	
+	/* ---------------------------------------------- /*
+	 * Home BG
+	/* ---------------------------------------------- */
+
+	$(".screen-height").height($(window).height());
+
+	$(window).resize(function(){
+		$(".screen-height").height($(window).height());
+	});
+
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		$('#home').css({'background-attachment': 'scroll'});
+	} else {
+		$('#home').parallax('50%', 0.1);
+		}
+	
+	
+})(jQuery);
